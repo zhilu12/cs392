0df7b67c039e8890d235c1e887d6437221c136c3
Zhi Lin Lu <zhilinlu12@gmail.com>
2025-01-29 22:45:02 -0500

    implemented nth_prime and isPrime helper function

--- hw/hw1/src/main.rs
+++ hw/hw1/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     assert!(is_close(d, 2.0_f64.sqrt()));
 
 
-    println!(nth_prime(22));
+    println!("{}", nth_prime(22));
     // Problem 2
 
 
@@ -20,26 +20,51 @@ fn is_close(num1: f64, num2: f64) -> bool {
     (num1 - num2).abs() < 1e-10
 }
 
-fn nth_prime(n: u32) -> u32 {
-
+fn nth_prime(mut n: u32) -> u32 {
+    n += 1;
     //brute force
-    let mut count = 1;
-    if n = 0 {
-        2
-    }
-    else {
-        while count < n {
-            let i = 0;
-            while i < count {
-                
-                count += 1;
-            }
+    let mut i: u32 = 2;
+    while n > 0 {
+        if isPrime(i) {
+            n -= 1;
         }
+        i += 1;
     }
 
+    i - 1
     // sieving primes
     
-
-
-    
 }
+
+// Checks if the given number is prime
+fn isPrime(num: u32) -> bool {
+    // cases for 1, 2, and 3
+    if num <= 1 {
+        return false;
+    }
+    if num == 2 || num == 3 {
+        return true;
+    }
+
+    // if the num is divisible by 2 or 3
+    if num % 2 == 0 || num % 3 == 0 {
+        return false;
+    }
+
+    let mut i = 5;
+    while i * i <= num {
+        if num % i == 0 || num % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+    true
+}
+/*
+fn is_taxicab(n: u32) -> bool {
+
+
+
+
+}
+*/
