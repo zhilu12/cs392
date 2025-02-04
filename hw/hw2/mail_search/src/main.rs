72c82fba408860d8bc5fa28ad1e3dc74bff804e5
Zhi Lin Lu <zhilinlu12@gmail.com>
2025-02-04 12:54:36 -0500

    uploading email_from_string, revert_from_munge, and get_email_read_to_string. email_from_str() not yet implemented correctly. Some questions on purpose of get_email_read_to_string(), but seems to be clarified more in section 3

--- hw/hw2/mail_search/src/main.rs
+++ hw/hw2/mail_search/src/main.rs
@@ -5,3 +5,34 @@ fn main() {
 fn mail_search(email: &str) -> bool {
     false
 }
+
+fn email_from_str(mbox: &str) -> email:String {
+    let mut email = String::new();
+
+    // will need to implement going over emails in mbox and checking for From-line
+
+    for email in mbox {
+        if mail_search(email) {
+            email
+        }
+        else {
+            panic
+        }
+    }
+}
+
+fn revert_from_munge(line: &str) -> &str {
+    if line.starts_with(">From") {
+        &line[1..]
+    }
+    else {
+        line
+    }
+}
+
+// is this function getting a singular email and converting it to a string?
+// or the whole mail box and converting
+fn get_email_read_to_string(filename: &str) -> email: String {
+    let mbox: String = fs::read_to_string(filename)?;
+    email_from_str(&mbox)
+}
