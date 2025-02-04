74e2866530e97a79675b0065eec7a5cc86da17d4
Zhi Lin Lu <zhilinlu12@gmail.com>
2025-02-04 13:15:31 -0500

    getting started on get_email_bufread_lines

--- hw/hw2/mail_search/src/main.rs
+++ hw/hw2/mail_search/src/main.rs
@@ -6,7 +6,7 @@ fn mail_search(email: &str) -> bool {
     false
 }
 
-fn email_from_str(mbox: &str) -> email:String {
+fn email_from_str(mbox: &str) -> String {
     let mut email = String::new();
 
     // will need to implement going over emails in mbox and checking for From-line
@@ -32,7 +32,17 @@ fn revert_from_munge(line: &str) -> &str {
 
 // is this function getting a singular email and converting it to a string?
 // or the whole mail box and converting
-fn get_email_read_to_string(filename: &str) -> email: String {
+fn get_email_read_to_string(filename: &str) -> String {
     let mbox: String = fs::read_to_string(filename)?;
     email_from_str(&mbox)
+}
+
+// Need to implement iterator over the lines and check for From-lines
+fn get_email_bufread_lines(filename: &str) -> String {
+    let file = File::open(filename);
+    let mut buf_reader = BufReader::new(file);
+   
+
+
+
 }
