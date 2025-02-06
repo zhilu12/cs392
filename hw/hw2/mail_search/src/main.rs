a3ef9c7dc2e032515053fd3c405d7d1e4792b5b1
Zhi Lin Lu <zhilinlu12@gmail.com>
2025-02-06 18:09:08 -0500

    added code for problems 3 and 4

--- hw/hw2/mail_search/src/main.rs
+++ hw/hw2/mail_search/src/main.rs
@@ -8,23 +8,29 @@ fn condition(email: &str) -> bool {
 
 fn email_from_str(mbox: &str) -> String {
     let mut email = String::new();
+    let mut collecting = false;
 
     // will need to implement going over emails in mbox and checking for From-line
     // iterate over each line, creating the email based on if From-line exists
 
     for line in mbox.lines() {
         if line.starts_with("From") {
-            if condition(&email) {
-                email
-            } else {
-                email = "";
-                email.push_str(line);
+            if collecting && !email.is_empty() {
+                if condition(&email) {
+                    return email;
+                }
+                email.clear();
             }
+            collecting = true;
+        } else if collecting {
+            email.push_str(revert_from_munge(line));
         }
-        email.push_str(line);
     }
-    if !condition(email) {
-        panic
+
+    if condition(email) {
+        return email;
+    } else {
+        panic!();
     }
 }
 
@@ -47,4 +53,55 @@ fn get_email_read_to_string(filename: &str) -> String {
 fn get_email_bufread_lines(filename: &str) -> String {
     let file = File::open(filename);
     let mut buf_reader = BufReader::new(file);
+
+    let email = String::new();
+    let mut collecting = false;
+
+    for line in buf_reader.lines() {
+        if line.starts_with("From") {
+            if collecting && !email.is_empty() {
+                if condition(&email) {
+                    return email;
+                }
+                email.clear();
+            }
+            collecting = true;
+        }
+        if collecting {
+            email.push_str(revert_from_munge(line));
+        }
+    }
+
+    if condition(email) {
+        return email;
+    } else {
+        panic!();
+    }
+}
+
+fn get_email_bufread_read_line(filename: &str) -> String {
+    let file = File::open(filename);
+    let mut buf_reader = BufRead::new(file);
+
+    let mut buf = String::new();
+    let mut email = String::new();
+
+    // file not done not implemeted
+    // when number of bytes is not 0?
+    while (buf_reader.read_line(&mut buf) != 0) {
+        // double reading?
+        buf_reader.read_line(&mut buf);
+        if buf.starts_with("From") {
+            if condition(email) {
+                return email;
+            }
+            email.clear();
+        }
+        email.push_str(revert_from_munge(buf))
+    }
+
+    if condition(email) {
+        return email;
+    }
+    panic!();
 }
