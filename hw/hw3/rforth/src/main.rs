4bd6d5d7e47cce948c963e5c3f3d980cd9f0ad71
Zhi Lin Lu <zhilinlu12@gmail.com>
2025-02-16 16:58:27 -0500

    finished implementation of eval_word and eval_command. Finished implementing REPL in main.

--- hw/hw3/rforth/src/main.rs
+++ hw/hw3/rforth/src/main.rs
@@ -1,5 +1,43 @@
+use std::collections::HashMap;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut config = Config::new();
+    let stdin = io::stdin();
+
+    println!("Type `bye` to exit");
+
+    loop {
+        print!(">> ");
+        io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        // read line from stdin
+        if stdin.read_line(&mut line).unwrap() == 0 {
+            break;
+        }
+        let entry = line.trim();
+
+        // if bye, exit
+        if entry == "bye" {
+            break;
+        }
+
+        // split line into elements by whitespace
+        let words: Vec<&str> = entry.split_whitespace().collect();
+        let mut error_occurred = false;
+        for word in words {
+            // if a word is bye, exit
+            if word == "bye" {
+                return;
+            }
+            // convert &str to String before calling eval_word
+            if let Err(e) = config.eval_word(word.to_string()) {
+                println!("{}", e);
+                break;
+            }
+        }
+    }
 }
 
 type Program = Vec<Command>;
@@ -54,83 +92,119 @@ impl Config {
         }
     }
 
-    fn eval_word(word: String) {
-        if let some(program) = self.dict.get(&word) {
+    fn eval_word(&mut self, word: String) -> Result<(), String> {
+        if let Some(program) = self.dict.get(&word) {
+            let program = program.clone();
             for command in program {
-                self.eval_command(command);
+                self.eval_command(command)?;
             }
-        }
-    } else {
-        if let Ok(n) = word.parse::<i32> {
+            Ok(())
+        } else if let Ok(n) = word.parse::<i32>() {
             self.eval_command(Command::Push(n))
         } else {
-            Err()
+            Err(format!("undefined word: {}", word))
         }
     }
 
-    fn eval_command(&mut self, command: Command) {
+    fn eval_command(&mut self, command: Command) -> Result<(), String> {
         match command {
             Command::Push(n) => {
                 self.stack.push(n);
+                Ok(())
             }
             Command::Drop => {
-                if stack.len() > 0{
-                    self.stack.pop();
+                if let Some(top) = self.stack.pop() {
+                    println!("{}", top);
+                    Ok(())
                 } else {
-                    Err("No elements on the stack")
+                    Err("stack underflower".to_string())
                 }
             }
             Command::Swap => {
-                let len = stack.len();
-                if len > 2 {
-                    self.stack.swap(len - 2, len -1)
+                let len = self.stack.len();
+                if len >= 2 {
+                    self.stack.swap(len - 2, len - 1);
+                    Ok(())
                 } else {
-                    Err("Less than two elements on the stack")
+                    Err("stack underflow".to_string())
                 }
             }
             Command::Print => {
-                if let Some(top) = stack.pop() {
+                if let Some(top) = self.stack.pop() {
                     println!("{}", top);
+                    Ok(())
                 } else {
-                    Err("No elements on stack")
+                    Err("stack underflow".to_string())
                 }
             }
             Command::PrintStack => {
-                if stack.len() > 0 {
-                    for Some(n) in stack {
-                        println!("{} ", n)
-                    }
-                } else {
-                    Err("No elements on stack")
+                for val in &self.stack {
+                    print!("{} ", val);
                 }
+                println!();
+                Ok(())
             }
             Command::Add => {
-                // type checking for intergers?
-                // Won't work for 2 elements if one of them is an operation
-                if stack.len() > 2 {
-                    let n = stack.pop();
-                    let m = stack.pop();
-                    stack.push(n + m);
+                if self.stack.len() < 2 {
+                    Err("stack underflow".to_string())
                 } else {
-                    Err("Less than two elements on the stack")
+                    let b = self.stack.pop().unwrap();
+                    let a = self.stack.pop().unwrap();
+                    self.stack.push(a + b);
+                    Ok(())
                 }
             }
             Command::Sub => {
-                if stack.len() > 2 {
-
+                if self.stack.len() < 2 {
+                    Err("stack underflow".to_string())
+                } else {
+                    let b = self.stack.pop().unwrap();
+                    let a = self.stack.pop().unwrap();
+                    self.stack.push(a - b);
+                    Ok(())
                 }
             }
             Command::Mul => {
-
+                if self.stack.len() < 2 {
+                    Err("stack underflow".to_string())
+                } else {
+                    let b = self.stack.pop().unwrap();
+                    let a = self.stack.pop().unwrap();
+                    self.stack.push(a * b);
+                    Ok(())
+                }
             }
-            Command::Div {
-
+            Command::Div => {
+                if self.stack.len() < 2 {
+                    Err("stack underflow".to_string())
+                } else {
+                    let b = self.stack.pop().unwrap();
+                    if b == 0 {
+                        return Err("division by zero".to_string());
+                    }
+                    let a = self.stack.pop().unwrap();
+                    self.stack.push(a / b);
+                    Ok(())
+                }
             }
-            Command::Mod {
-
+            Command::Mod => {
+                if self.stack.len() < 2 {
+                    Err("stack underflow".to_string())
+                } else {
+                    let b = self.stack.pop().unwrap();
+                    if b == 0 {
+                        return Err("division by zero".to_string());
+                    }
+                    let a = self.stack.pop().unwrap();
+                    self.stack.push(a % b);
+                    Ok(())
+                }
             }
         }
     }
+
 }
+
+
 
 
