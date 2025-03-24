6dcf50bbd3bb78c6c5a76788cfba441650ef43d3
Zhi Lin Lu <zhilinlu12@gmail.com>
2025-03-23 20:31:50 -0400

    finished up iterator for lexer and implemented the parser. Parser goes through tokens returned by the lexer and parses into lists. Checks for errors such as extra, unclosed, or open parens.

--- hw/hw5/target/debug/deps/hw5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\zhili\cs392\hw\hw5\target\debug\deps\hw5.exe: src\main.rs
-
-C:\Users\zhili\cs392\hw\hw5\target\debug\deps\hw5.d: src\main.rs
-
-src\main.rs:
--- hw/hw5/target/debug/hw5.d
+++
@@ -1,1 +0,0 @@
-C:\Users\zhili\cs392\hw\hw5\target\debug\hw5.exe: C:\Users\zhili\cs392\hw\hw5\src\main.rs
